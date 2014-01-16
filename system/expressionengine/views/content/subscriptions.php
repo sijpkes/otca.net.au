48cdb725e537edcffe11cd299d6fec72958d2b08
Paul Sijpkes <ps158@cdb206-c02g7s98.newcastle.edu.au>
2014-01-16 11:35:52 +1100

    formatting changes

--- system/expressionengine/views/content/subscriptions.php
+++ system/expressionengine/views/content/subscriptions.php
@@ -1,6 +1,6 @@
 <?php extend_template('default') ?>
 	
-	<h3><?=$institution_name?> Subscription Information</h3>
+	<h2><?=$institution_name?> Subscription Information</h2>
 	<?php if($expired === TRUE):  ?>
 		<p style='color: red'><strong><?= $institution_name ?> does not currently have an active subscription</strong>.  New students will not be able to register.</p>
 	<?php endif; ?>	
@@ -8,8 +8,9 @@
 	<p>Your subscription expires on: <b><?= $expiry_date ?></b></p>
 		<ul class="bullets">
 			
-			<li><p>
-				Student registration link:<br>
+			<li>
+				<h3>Student registration link</h3>
+				<p>
 				Place this link on the course page for students using <?=$institution_name?>'s LMS.
 				<textarea>
 					<?=$student_url?>
@@ -17,9 +18,10 @@
 				</p>
 			</li>
 			
-			<li><p>
-				Educator registration link:<br>
-				Paste this link into an Email to Practice Educators.
+			<li>
+				<h3>Educator registration link</h3>
+				<p>
+				Paste this link into an email to the practice educators that will observe students.
 				<textarea>
 					<?=$educator_url?>
 				</textarea>

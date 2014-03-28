ec18dc94b39973a1ca2ac0aef6acef084b824582
Paul Sijpkes <ps158@cdb206-c02g7s98.newcastle.edu.au>
2014-03-28 12:57:49 +1100

    Final Github Commit - this project is now closed.

--- js/register-message.js
+++ js/register-message.js
@@ -24,8 +24,8 @@
 $(document).ready(function() {
 	
 	$('body').append("<div class='global_file-viewer' style='display:none'><span id='content'></span></div>");
-	$('div.global_file-viewer > span#content').append("<h1 style=\"color: #663399; text-align:center\">Please <a href=\"/member/login\">login</a> or <a href=\"/member/register\">register</a>.</h1>\
-							  <p>To access the diary, OTCEM and ePortfolio you will need to <a href=\"/member/login\">login</a> or <a href=\"/member/register\">register</a>.\
+	$('div.global_file-viewer > span#content').append("<h1 style=\"color: #663399; text-align:center\">Please <a href=\"/member/login\">login</a></h1>\
+							  <p>To access the diary, OTCEM and ePortfolio you will need to <a href=\"/member/login\">login</a> or contact your intstitution's<a href=\"/partners/web-admin-list\">OTCA administrator</a> for information on how to register.\
 							  </p><p>You can browse and view the videos and documents under the Practice Resources, Practice Contexts, Occupational Therapy Practice Process\
 							  and some of the links under Evidencing without logging in.</p>").
 				parent().css({height: "156px"}).show(100);
